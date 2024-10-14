6728a4a2e8659c62617fae54bc02184d3d649961
itthiporn402 <itthiporn7234@gmail.com>
2024-10-15 00:03:54 +0700

    2.1

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
-import './placeOrder.css';
+import './PlaceOrder.css';
 
 const placeOrder = () => {
 
